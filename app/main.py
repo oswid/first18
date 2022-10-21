6d36435837db867c07550219cd323b808134ced9
olaf <vol.umes@mail.ru>
2022-10-21 17:04:27 +0300

    hitin layout

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -10,11 +10,10 @@ templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory=str(Path(BASE_DIR, 'static'))), name="static")
 
-#templates = Jinja2Templates(directory="./app/templates")
 
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("layout_rus.html", {"request": request})
 
